cf2b5f855097d929468c22597d8349446fc5e5df
Leah <shlemov@tuta.io>
2022-08-09 10:59:04 +0300

    Added comments

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
+/* 
+ * This progran uses sababot.cpp functions in order to
+ * display and run SababotGL.
+ */
 #include "sababot.h"
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void InitGlut(int argc, char **argv)
+/*
+ * Initializes window, sets some defaults and loads textures to buffer.
+ * To be called from the main function.
+ */
+void Init(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(START_WIDTH, START_HEIGHT);
     glutCreateWindow("SababotGL");
-}
 
-void Init()
-{
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0f, 0.0f, 0.2f, 0.0f);
     glShadeModel(GL_SMOOTH);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_DEPTH_TEST);
@@ -21,31 +26,57 @@ void Init()
     initTextures();
 }
 
+/*
+ * Display callback function
+ */
 void Display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Sets camera and frustum
     initCamera();
+
+    // Sets light source
     initLight();
+
+    // The position (and sometimes size) of the elements is relative to the floor size
     displayfloor(FLOOR_SIZE);
-    displayWalls(FLOOR_SIZE, FLOOR_SIZE * 0.3);
-    displayTable(-TABLE_SIZEX * 0.5, FLOOR_SIZE * 0.5 - 20.0, TABLE_HEIGHT, TABLE_SIZEX, TABLE_SIZEZ, TABLE_THICKNESS);
-    displayTeapot(0.0, TABLE_HEIGHT + 2.0, FLOOR_SIZE * 0.5 - 15.0, 45.0);
-    displayChair(-CHAIR_SEAT_WIDTH * 0.5, FLOOR_SIZE * 0.5 - 20.0 - 2 * CHAIR_SEAT_DEPTH, TABLE_HEIGHT - 4.0, CHAIR_SEAT_WIDTH, CHAIR_SEAT_DEPTH, TABLE_THICKNESS);
-    displayFridge(-FLOOR_SIZE * 0.2, -FLOOR_SIZE * 0.5, FRIDGE_WIDTH, FRIDGE_HEIGHT, FRIDGE_DEPTH);
-    displayBin(-5.0, -FLOOR_SIZE * 0.5 + 5.0, 5.0, 5.0);
+    displayWalls(FLOOR_SIZE, FLOOR_SIZE * 0.3f);
+
+    // The table is located 20 units from the +Z wall, where x=0
+    displayTable(-TABLE_SIZEX * 0.5f, FLOOR_SIZE * 0.5f - 20.0f, TABLE_HEIGHT, TABLE_SIZEX, TABLE_SIZEZ, TABLE_THICKNESS);
+
+    // The teapot is located on the table, right in the middle
+    displayTeapot(0.0f, TABLE_HEIGHT + 2.0f, FLOOR_SIZE * 0.5f - 15.0f, 45.0f);
+
+    // The chair is located twice-its-depth units from the table, where x=0
+    displayChair(-CHAIR_SEAT_WIDTH * 0.5f, FLOOR_SIZE * 0.5f - 20.0f - 2.0f * CHAIR_SEAT_DEPTH, TABLE_HEIGHT - 4.0f, CHAIR_SEAT_WIDTH, CHAIR_SEAT_DEPTH, TABLE_THICKNESS);
+
+    // The fridge is located 0.2*FLOOR_SIZE units towards the -X wall
+    displayFridge(-FLOOR_SIZE * 0.2f, -FLOOR_SIZE * 0.5f, FRIDGE_WIDTH, FRIDGE_HEIGHT, FRIDGE_DEPTH);
+
+    // The garbage bin is located 15 units from the fridge towards the +X wall
+    displayBin(-FLOOR_SIZE * 0.2f + 15.0, -FLOOR_SIZE * 0.5f + 5.0f, 5.0f, 5.0f);
+
     displayRobot();
     displayMovingMode();
-    displayAdjustAmbient();
-    displayHelp();
+    displayAdjustAmbient(); // displays only if needed
+    displayHelp(); // displays only if needed
     glutSwapBuffers();
 }
 
+/*
+ * Reshape callback function
+ */
 void Reshape(GLsizei w, GLsizei h)
 {
     updateWindowAspect(1.0 * w / h);
     glViewport(0, 0, w, h);
 }
 
+/*
+ * Right-click menu
+ */
 void Menu(int value)
 {
     switch (value)
@@ -76,8 +107,7 @@ void RegisterCallbacks()
 
 int main(int argc, char **argv)
 {
-    InitGlut(argc, argv);
-    Init();
+    Init(argc, argv);
     RegisterCallbacks();
 
     glutCreateMenu(Menu);
